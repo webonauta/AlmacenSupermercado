b8e57d0a1e0380fce1b35fec0cae93dc48b5cdce
EmmaVilla90 <emmanuel.navarro@estudiante.uacm.edu.mx>
2022-03-06 21:52:44 -0600

    Conexión de Base de Datos
    Se agregó conexión a base de datos con  patrón de diseño Singleton

--- src/main/java/DAO/Conexion.java
+++ src/main/java/DAO/Conexion.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
- * @author Alberto
+ * @author Navarro Villa Emmanuel De Jesús
  */
 public class Conexion {
+    private static Connection con=null;
+    private static String driver="com.mysql.cj.jdbc.Driver";
+    private static String password="qqQN9kixg4";
+    private static String usuario="sql3477143";
+    private static String bd="sql3477143";
+    private static String url="jdbc:mysql://sql3.freesqldatabase.com/"+bd;
+
+
+public static Connection getConnection(){
+
+        try {  
+             if(con==null){
+                Runtime.getRuntime().addShutdownHook(new MiShDown());
+                Class.forName(driver);
+                con=DriverManager.getConnection(url,usuario,password);
+             }
+            } catch (ClassNotFoundException | SQLException ex) {
+           
+                Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+              }
+    return con;
+}
     
+static class MiShDown extends Thread{
+
+    public void run(){
+
+        try {
+            con.close();
+            System.out.println("Conexion Cerrada");
+        } catch (SQLException ex) {
+                Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+      }    
 }
