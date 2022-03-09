f8ee5396eca47b062646e792cf2fb20314313b9f
Alberto Juan Gabino <intrunet@gmail.com>
2022-03-09 00:15:20 -0600

    Navegación de menú
    Se agrega funcionalidad de navegación entre ventanas del menú hacia las vistas de clientes, productos y proveedores.

--- src/main/java/Controller/VistaMenuController.java
+++ src/main/java/Controller/VistaMenuController.java
@@ -6,7 +6,12 @@ package Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -22,5 +27,56 @@ public class VistaMenuController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    @FXML
+    private void btnMenuClientes(){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/VistaClientes.fxml"));
+            
+            Parent root = loader.load();
+            
+            VistaMenuController controlador = loader.getController();
+            
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+        }
+    }
+    
+    @FXML
+    private void btnMenuProductos(){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/VistaProductos.fxml"));
+            
+            Parent root = loader.load();
+            
+            VistaMenuController controlador = loader.getController();
+            
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+        }
+    }
+    
+    @FXML
+    private void btnMenuProveedores(){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/VistaProovedores.fxml"));
+            
+            Parent root = loader.load();
+            
+            VistaMenuController controlador = loader.getController();
+            
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+        }
+    }
     
 }
