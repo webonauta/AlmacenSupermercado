ab4b647b310ee9a1dd1336ecbfa10c41d305ffaf
Alberto Juan Gabino <intrunet@gmail.com>
2022-03-08 23:48:24 -0600

    Agregando archivos a packete DAO y Model
    Se agregan archivos ClientesDAO con la conexion a la base de datos y ClienteDTO a los paquetes DAO y Model

---
+++ src/main/java/DAO/ClientesDAO.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import Model.ClienteDTO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Alberto
+ */
+public class ClientesDAO {
+    private Connection conexion;
+    private PreparedStatement pstm;
+    private ResultSet rs;
+    private String sql;
+    
+    private void insertaCliente(ClienteDTO cliente){
+        try {
+            conexion = Conexion.getConnection();
+            conexion.setAutoCommit(false);
+            
+            sql = "INSERT INTO clientes(nombre, apellido_paterno, apellido_materno, telefono, direccion)";
+            sql += " VALUES(?, ?, ?, ?, ?);";
+            
+            pstm=conexion.prepareStatement(sql);
+            
+            pstm.setString(1, cliente.getNombre());
+            pstm.setString(2, cliente.getApellidoPaterno());
+            pstm.setString(3, cliente.getApellidoMaterno());
+            pstm.setString(4, cliente.getTelefono());
+            pstm.setString(5, cliente.getDireccion());
+            
+            int resultado = pstm.executeUpdate();
+            if(resultado > 0){
+                conexion.commit();
+                System.out.println("Cliente agregado correctamente");
+            }else{
+                throw new RuntimeException("No se pudo agregar los datos del cliente");
+            }
+            
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }finally{
+            try {
+                if(conexion != null) conexion.rollback();
+                if(pstm != null) pstm.close();
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+             
+            
+        }
+    }
+}
---
+++ src/main/java/Model/ClienteDTO.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Alberto
+ */
+public class ClienteDTO {
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String telefono;
+    private String direccion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    @Override
+    public String toString(){
+        return "ClienteDTO{" + "nombre=" + nombre + ", apellido paterno=" + apellidoPaterno + ", apellido materno=" + apellidoMaterno + ", telefono=" + telefono + ", direccion= " + direccion +'}';
+    }
+    
+}
